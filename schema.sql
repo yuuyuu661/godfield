8b7d70a49cbda79186efe09bc7fc22dc87fee630
yuuyuu661 <liuliliuli71@gmail.com>
2025-10-13 15:36:28 +0900

    Delete schema.sql

--- schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS entries (
-  id SERIAL PRIMARY KEY,
-  team_name TEXT NOT NULL,
-  member1 TEXT,
-  member2 TEXT,
-  member3 TEXT,
-  created_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
